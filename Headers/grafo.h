34a04d457627215ffac8acccbd5d6a93cc32ea26
Gabriel Silva <gasilva@inf.ufpel.edu.br>
2026-02-03 14:20:13 -0300

    Enhance Grafo class with name-ID association methods
    Added methods to associate names with IDs and retrieve IDs by name.

--- Headers/grafo.h
+++ Headers/grafo.h
@@ -13,9 +13,14 @@ struct Aresta {
 class Grafo {
 private:
     std::unordered_map<long long, std::vector<Aresta>> listaAdj; //Mapa: ID do nó -> Lista de Arestas
+
+    std::unordered_map<std::string, std::vector<long long>> mapaNomes;
 public:
     Grafo();
     void adicionarAresta(long long u, long long v, double peso);
     double dijkstra(long long primeiroNodo, long long ultimoNodo);
+    void associarNomeAoId(std::string nome, long long id); // Usada pelo DataManager
+    long long getIdByName(QString nomeRua); // Usada pela Interface Gráfica
 };
+
 
