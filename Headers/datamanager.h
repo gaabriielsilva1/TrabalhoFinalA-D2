61b94aee7c9d2fc15b45df844427b529c3923459
Gabriel Silva <gasilva@inf.ufpel.edu.br>
2026-02-03 14:27:53 -0300

    Update datamanager.h

--- Headers/datamanager.h
+++ Headers/datamanager.h
@@ -1,4 +1,16 @@
-#ifndef DATAMANAGER_H
+ifndef DATAMANAGER_H
 #define DATAMANAGER_H
+
+#include <QString>
+#include "grafo.h"
+#include "trie.h"
+
+class DataManager {
+public:
+    DataManager();
+    
+    // Retorna true se carregou com sucesso, false se deu erro no arquivo
+    bool carregarDados(const QString& caminhoArquivo, Grafo& grafo, Trie& trie);
+};
 
 #endif // DATAMANAGER_H
